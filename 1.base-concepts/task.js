1d5aaae3127dbbd6a5c591b3d75805428845b5fe
Ksenia Grom <gca-2001@mail.ru>
2025-09-11 16:53:51 +0300

    Task  completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,22 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = b**2 - 4 * a * c;
+  let d = b ** 2 - 4 * a * c;
   if (d < 0) {
     arr = [];
   } else if (d === 0) {
     arr = [-b / (2 * a)];
   } else {
-    arr = [(-b + Math.sqrt(d) )/(2 * a), (-b - Math.sqrt(d) )/(2 * a)];
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let mountPercent = percent / 100 / 12;
+  let sumCredit = amount - contribution;
+  let mountPay = sumCredit * (mountPercent + (mountPercent / (((1 + mountPercent) ** countMonths) - 1)));
+  let totalPay = mountPay * countMonths;
+  return Number(totalPay.toFixed((2)));
 }
