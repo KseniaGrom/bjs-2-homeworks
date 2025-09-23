60809a44043f77d4852fc89a2151380bfc4ace28
Ksenia Grom <gca-2001@mail.ru>
2025-09-23 08:55:13 +0300

    Tasks completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(!this.hasOwnProperty("marks")) {
+    console.log("Студент отчислен");
+    return;
+  }
+  this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(!this.marks || this.marks.length ===0){
+    return 0;
+  }
+  let srArif = 0;
+  for(const mark of this.marks){
+    srArif = srArif + mark;
+  }
+  return srArif/this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+
+  this.excluded = reason;
 }
